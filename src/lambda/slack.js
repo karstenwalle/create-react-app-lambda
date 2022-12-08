7b5ff9a4e70a5e0b76eed8f75b621276c2cf3f45
Karsten Walle-Hansen <karstenwallehansen@gmail.com>
2022-12-08 18:23:29 +0100

    bug fixing

--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -8,8 +8,7 @@ export function handler(event, context, callback) {
     const payload = JSON.parse(event.body);
     fetch(slackURL, {
       method: "POST",
-      // body: {"text": "This is a line of text in a channel.\nAnd this is anotherd line of text."}
-      payload: JSON.stringify({ text: payload.text })
+      body: JSON.stringify({ text: payload.text })
     }).then(() => {
       callback(null, { statusCode: 204 });
     }).catch((e) => {
