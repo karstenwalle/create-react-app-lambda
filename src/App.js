8efccb474f4969df0048acbc408dd160ea0eb6c1
Karsten Walle-Hansen <karstenwallehansen@gmail.com>
2022-12-08 20:29:17 +0100

    bug fixing'

--- src/App.js
+++ src/App.js
@@ -56,9 +56,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Slack Messenger</h1>
         </header>
-        {/* <p onClick={this.handleIdentity}>User Status</p> */}
-        {/* <p><button onClick={this.handleIdentity}>User Status</button></p> */}
-        <SlackMessage/>
+        <p><button onClick={this.handleIdentity}>User Status</button></p>
+        <SlackMessage />
       </div>
     );
   }
